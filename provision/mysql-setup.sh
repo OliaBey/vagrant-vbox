0f59b7d5571a156051155128aefd9aad3172547c
Olia Bey <oliabey11@gmail.com>
2019-02-05 21:05:22 +0200

    v2 demo1

--- provision/mysql-setup.sh
+++ provision/mysql-setup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+set +x
 
-apt-get install mysql
-
+sudo yum -y update || exit 1
+sudo yum -y install wget || exit 2
+sudo wget http://dev.mysql.com/get/mysql80-community-release-el7-2.noarch.rpm || exit 3
+sudo rpm -Uvh mysql80-community-release-el7-2.noarch.rpm || exit 4
+sudo yum -y install mysql-server || exit 5
+sudo yum -y install mysql-community-server || exit 6
+sudo systemctl start mysqld || exit 7
+sudo systemctl status mysqld || exit 8
 exit 0
--- provision/xwiki-setup.sh
+++ provision/xwiki-setup.sh
@@ -1,4 +1,24 @@
-#!/USR/bin/env bash
-set -x
+#!/usr/bin/env bash
+set +x
 
-sudo apt-get install xwiki
+sudo apt-get install -y screen || exit 5
+sudo apt-get update && sudo apt-get upgrade -y || exit 6
+sudo apt-get install -y tomcat8 || exit 7
+wget -q "http://maven.xwiki.org/public.gpg" -O- | sudo apt-key add - || exit 8
+##sudo wget "http://maven.xwiki.org/stable/xwiki-stable.list" -P /etc/apt/sources.list.d/ || exit 9
+sudo wget -P /etc/apt/sources.list.d/ http://maven.xwiki.org/stable/xwiki-stable.list || exit 9
+sudo apt-get update ||exit 10
+sudo apt-get install -y xwiki-enterprise-tomcat8-mysql || exit 11
+sudo mkdir /var/lib/tomcat8/bin
+sudo touch /var/lib/tomcat8/bin/setenv.sh || exit 12
+sudo chown vagrant:vagrant /var/lib/tomcat8/bin/setenv.sh
+sudo echo '#!/bin/sh' > /var/lib/tomcat8/bin/setenv.sh
+sudo echo 'export JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true"' >> /var/lib/tomcat8/bin/setenv.sh
+sudo echo 'CATALINA_OPTS="-Xms512M -Xmx1024M -XX:PermSize=512M -XX:MaxPermSize=512M"' >> /var/lib/tomcat8/bin/setenv.sh || exit 13
+sudo /etc/init.d/tomcat8 restart
+
+
+
+
+exit 0
+
